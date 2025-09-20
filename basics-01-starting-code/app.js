25f1b09704eb1d1bc138f73b6025bd5889660e1d
huntmori <huntmori04x22@gmail.com>
2025-09-21 05:22:24 +0900

    19. Vue 앱의 "메서드" 이해하기

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -5,8 +5,17 @@ const app = window.Vue.createApp({
             vueLink: 'https://vuejs.org/',
         };
     },
+    methods: {
+        outputGoal: function() {
+            const randomNumber = Math.random();
+
+            if(randomNumber < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Master Vue!';
+            }
+        }
+    },
 });
-
-
 
 app.mount('#user-goal');
