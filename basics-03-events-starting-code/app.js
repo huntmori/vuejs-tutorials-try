0deeee02191aafa3bdce0b5d9655d7bb935c6d84
Nhyun Kim <nhyun5050@gmail.com>
2025-09-23 07:06:40 +0900

    24. 이벤트 및 메서드

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+    },
+    remove() {
+      this.counter = this.counter - 1;
+    },
+    reset() {
+      this.counter = 0;
+    },
+  }
+});
+
+app.mount('#events');
