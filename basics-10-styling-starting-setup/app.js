dd6742b084d681a39cab4dd18a7da3d4e2f8af99
huntmori <huntmori04x22@gmail.com>
2025-09-27 16:22:16 +0900

    37. 클래스 및 연산(Computed) 프로퍼티

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -17,6 +17,23 @@ const app = Vue.createApp({
 
             }
         }
+    },
+    computed: {
+        boxAClasses() {
+            return {
+                active: this.boxASelected
+            }
+        },
+        boxBClasses() {
+            return {
+                active: this.boxBSelected
+            }
+        },
+        boxCClasses() {
+            return {
+                active: this.boxCSelected
+            }
+        }
     }
 });
 
