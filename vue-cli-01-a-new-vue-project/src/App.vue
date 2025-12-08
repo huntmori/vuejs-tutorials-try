f9e5405d5527f78274a8c0ad9f5e8154051c33e3
huntmori <huntmori04x22@gmail.com>
2025-12-08 21:57:55 +0900

    88. 기본 Vue 앱 만들기

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+      <h2>My Friends</h2>
+      <ul>
+        <li v-for="friend in friends" :key="friend.id">
+          <h3>{{ friend.name }}</h3>
+          <p>Phone: {{ friend.phone }}</p>
+          <p>Email: {{ friend.email }}</p>
+        </li>
+      </ul>
+    </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        friends: [
+          {
+            id: 'manuel', 
+            name: 'Manuel Lorenz', 
+            phone: '0171 1234567', 
+            email: 'manuel@example.com'
+          },
+          {
+            id: 'julie', 
+            name: 'Julie Jones', 
+            phone: '0171 7654321', 
+            email: 'julie@example.com'
+          },
+        ]
+      }
+    },
+    methods: {},
+    computed: {}
+  };
+</script>
